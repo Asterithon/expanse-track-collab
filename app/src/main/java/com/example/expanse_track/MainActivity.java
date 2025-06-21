34e640f570ccfb39aee8763a3affe7ee496d9f06
ran <randyparadita31@gmail.com>
2025-06-21 22:30:11 +0800

    Add new features

--- app/src/main/java/com/example/expanse_track/MainActivity.java
+++ app/src/main/java/com/example/expanse_track/MainActivity.java
@@ -10,7 +10,9 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -20,14 +22,18 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private ListView lvTransactions;
+    private TextView tvTotal, tvRevenue, tvExpense;
     private ArrayList<Transaction> list = new ArrayList<>();
-    Button btnShowIncome, btnShowExpense, btnInput;
+    Button btnShowIncome, btnShowExpense;
+    ImageButton btnInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,10 +46,14 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-    btnInput = findViewById(R.id.btn_input);
-    btnShowExpense = findViewById(R.id.btn_show_expense);
-    btnShowIncome = findViewById(R.id.btn_show_income);
-    lvTransactions = findViewById(R.id.list_transactions);
+
+        tvTotal = findViewById(R.id.tv_total);
+        tvRevenue = findViewById(R.id.tv_revenue);
+        tvExpense = findViewById(R.id.tv_expense);
+        btnInput = findViewById(R.id.btn_input);
+        btnShowExpense = findViewById(R.id.btn_show_expense);
+        btnShowIncome = findViewById(R.id.btn_show_income);
+        lvTransactions = findViewById(R.id.list_transactions);
 
     lvTransactions.setOnItemClickListener(new AdapterView.OnItemClickListener() {
         @Override
@@ -84,9 +94,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void itemlongclick(int i) {
         Transaction t = list.get(i);
-        String[] menu = new String[] {"edit", "hapus"};
+        String[] menu = new String[] {"Edit", "Delete"};
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("ini title");
+        builder.setTitle("Action:");
         builder.setItems(menu, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int which) {
@@ -113,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         String sql = "DELETE FROM `transaction` WHERE `id` = ?;";
         db.execSQL(sql, new Object[] {t.getId()});
+        Toast.makeText(this, "Transaction Deleted Succesfully", Toast.LENGTH_LONG).show();
         refresh();
     }
 
@@ -121,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
         Intent goDetail = new Intent(this, DetailActivity.class);
         goDetail.putExtra("id", String.valueOf(t.getId()));
         startActivity(goDetail);
-        Toast.makeText(this, t.getDescription(), Toast.LENGTH_LONG).show();
+//        Toast.makeText(this, t.getDescription(), Toast.LENGTH_LONG).show();
         refresh();
     }
 
@@ -150,27 +161,58 @@ public class MainActivity extends AppCompatActivity {
         DbHelper dbHelper = new DbHelper(this);
         SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        String sql = "SELECT `amount`, `description`, `date`, `type`, `id` FROM `transaction` WHERE 1";
+        String sql = "SELECT `id`, `amount`, `description`, `date`, `type` FROM `transaction`";
         Cursor c = db.rawQuery(sql, new String[0]);
 
-        //ambil data dari dtbsase
+        //ambil data dari database
         list.clear();
         while(c.moveToNext()) {
-            int amount = c.getInt(0);
-            String description = c.getString(1);
-            String date = c.getString(2);
-            int type = c.getInt(3);
-            int id = c.getInt(4);
+            int id = c.getInt(0);
+            int amount = c.getInt(1);
+            String description = c.getString(2);
+            String date = c.getString(3);
+            int type = c.getInt(4);
 
             Transaction t = new Transaction(amount, description, date, type, id);
             list.add(t);
         }
 
         //update ke list bang
-        lvTransactions.setAdapter(new ArrayAdapter<Transaction>(this,
-                android.R.layout.simple_list_item_1, list));
-    }
+        TransactionAdapter adapter = new TransactionAdapter(this, list);
+        lvTransactions.setAdapter(adapter);
 
+
+        String sqlRevenue = "SELECT SUM(`amount`) AS total_revenue FROM `transaction` WHERE `type` = 1;";
+        String sqlExpense = "SELECT SUM(`amount`) AS total_expense FROM `transaction` WHERE `type` = 0;";
+        Cursor totalRev = db.rawQuery(sqlRevenue, null);
+        Cursor totalExp = db.rawQuery(sqlExpense, null);
+
+
+        double totalRevenue = 0;
+        if (totalRev.moveToFirst()) {
+            totalRevenue = totalRev.getDouble(0);
+        }
+        totalRev.close();
+
+        double totalExpense = 0;
+        if (totalExp.moveToFirst()) {
+            totalExpense = totalExp.getDouble(0);
+        }
+        totalExp.close();
+
+        // Update textview set total to format IDR
+        Locale localeID = new Locale("in", "ID");
+
+        NumberFormat formatNumber = NumberFormat.getNumberInstance(localeID);
+        String formattedRevenue = formatNumber.format(totalRevenue);
+        String formattedExpense = formatNumber.format(totalExpense);
+        String formattedBalance = formatNumber.format(totalRevenue - totalExpense);
+
+        tvRevenue.setText(formattedRevenue);
+        tvExpense.setText(formattedExpense);
+        tvTotal.setText("Rp " + formattedBalance);
+
+    }
 
 
 }
